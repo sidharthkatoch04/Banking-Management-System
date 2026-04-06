0c016846c3d7af3779e16a8e44be13cddea10d2d
sidharthkatoch04 <sidharthkatoch22@gmail.com>
2026-04-06 15:37:49 +0530

    Update Admin.cpp

--- Admin.cpp
+++ Admin.cpp
@@ -1,44 +1,45 @@
-#include <iostream>
 #include "Admin.hpp"
-#include "BankingOperations.hpp"
 
-using namespace std;
+void Admin::adminPanel() {
 
-void Admin::showProfile(){
-    cout<<"Admin Panel\n";
+    string pass;
+    cin >> pass;
+
+    if (pass != "admin123") return;
+
+    int choice;
+
+    do {
+        cin >> choice;
+
+        int id, index;
+
+        switch (choice) {
+
+        case 1:
+            cin >> id;
+            index = findUserById(id);
+            if (index != -1)
+                users[index].frozen = true;
+            break;
+
+        case 2:
+            cin >> id;
+            index = findUserById(id);
+            if (index != -1)
+                users[index].frozen = false;
+            break;
+
+case 3:
+cin >> id;
+index= findUserById(id);
+if(index!=-1){
+for(int i = index; i<usercount-1;i++)
+    users[i]= users[i+1];
+userCount--;
 }
-
-void Admin::freezeAccount(int id){
-
-    int index = BankingOperations::findUserById(id);
-
-    if(index!=-1){
-        users[index].frozen = true;
-        cout<<"Account frozen\n";
+break;
+        }
     }
-}
-
-void Admin::unfreezeAccount(int id){
-
-    int index = BankingOperations::findUserById(id);
-
-    if(index!=-1){
-        users[index].frozen = false;
-        cout<<"Account unfrozen\n";
-    }
-}
-
-void Admin::deleteUser(int id){
-
-    int index = BankingOperations::findUserById(id);
-
-    if(index==-1)
-        return;
-
-    for(int i=index;i<userCount-1;i++)
-        users[i]=users[i+1];
-
-    userCount--;
-
-    cout<<"User deleted\n";
+        while(choice !=0);
 }
