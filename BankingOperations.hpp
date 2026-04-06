4c21238c4739c2f83b21036175ad6a00c03c1737
sidharthkatoch04 <sidharthkatoch22@gmail.com>
2026-04-06 15:50:35 +0530

    Update BankingOperations.hpp by sidharth

--- BankingOperations.hpp
+++ BankingOperations.hpp
@@ -1,28 +1,12 @@
-#ifndef BANKINGOPERATIONS_H
-#define BANKINGOPERATIONS_H
+#ifndef BANKOPERATIONS_HPP
+#define BANKOPERATIONS_HPP
 
 #include "User.hpp"
 
-class BankingOperations {
-
+class BankOperations{
 public:
-
-    static int findUserByUsername(string username);
-    static int findUserById(int id);
-
-    static void registerUser();
-    static int login();
-
-    static void deposit(int index);
-    static void deposit(int index, double amount);   // function overloading
-
-    static void withdraw(int index);
-    static void transfer(int index);
-
-    static void checkBalance(int index);
-    static void viewTransactions(int index);
-
-    static void addTransaction(int index,string type,double amount,int otherId=0);
+void deposit(int index);
+void withdraw(int index);
+void transfer(int index);
+void viewTransactions(int index);
 };
-
-#endif
