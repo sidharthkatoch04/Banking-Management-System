7f72bf3c9f09d528479119c9925b3be848acac34
sidharthkatoch04 <sidharthkatoch22@gmail.com>
2026-05-04 23:02:14 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,11 @@
+#include "User.hpp"
+#include "BankOperations.hpp"
+#include "Admin.hpp"
 int main() {
-    loadUsers();
+    BankOperations bank;
+    Admin admin;
     int choice;
     do {
-        cout << "\n===== BANK SYSTEM =====\n";
-        cout << "1. Register\n";
-        cout << "2. Login\n";
-        cout << "3. Admin Panel\n";
-        cout << "0. Exit\n";
         cin >> choice;
         switch (choice) {
         case 1:
@@ -15,14 +14,24 @@ int main() {
         case 2: {
             int index = login();
             if (index != -1) {
-                userMenu(index);
+                int c;
+                do {
+                    cin >> c;
+                    switch (c) {
+                    case 1: bank.deposit(index); break;
+                    case 2: bank.withdraw(index); break;
+                    case 3: bank.transfer(index); break;
+                    case 4: bank.viewTransactions(index); break;
+                    }
+                } while (c != 0);
             }
             break;
         }
         case 3:
-            adminPanel();
+            admin.adminPanel();
             break;
         }
     } while (choice != 0);
+
     return 0;
 }
