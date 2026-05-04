c83a99143776fe0d1a772738887c5cbe0c3d9fe1
sidharthkatoch04 <sidharthkatoch22@gmail.com>
2026-05-04 22:54:25 +0530

    Update Admin.cpp

--- Admin.cpp
+++ Admin.cpp
@@ -1,35 +1,25 @@
 #include "Admin.hpp"
-
 void Admin::adminPanel() {
-
     string pass;
     cin >> pass;
-
     if (pass != "admin123") return;
-
     int choice;
-
     do {
         cin >> choice;
-
         int id, index;
-
         switch (choice) {
-
         case 1:
             cin >> id;
             index = findUserById(id);
             if (index != -1)
                 users[index].frozen = true;
             break;
-
         case 2:
             cin >> id;
             index = findUserById(id);
             if (index != -1)
                 users[index].frozen = false;
             break;
-
 case 3:
 cin >> id;
 index= findUserById(id);
