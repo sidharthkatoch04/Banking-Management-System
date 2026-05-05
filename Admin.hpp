19779d64603cfc96b7d916bff1224aecfa70d3c2
Saumil-Gupta <saumil.gupta.professional@gmail.com>
2026-05-05 11:08:26 +0530

    Update Admin.hpp

--- Admin.hpp
+++ Admin.hpp
@@ -2,7 +2,10 @@
 #define ADMIN_HPP
 #include "User.hpp"
 class Admin {
+private:
+    const string ADMIN_PASSWORD = "admin123";
 public:
     void adminPanel();
+    void viewAllUsers();
 };
 #endif
