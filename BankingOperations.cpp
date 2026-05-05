c9ed88d23ce000733668630fefbb8f737ac9b7f8
Saumil-Gupta <saumil.gupta.professional@gmail.com>
2026-05-05 08:26:04 +0530

    Update BankingOperations.cpp

--- BankingOperations.cpp
+++ BankingOperations.cpp
@@ -1,4 +1,4 @@
-#include "BankOperations.hpp"
+#include "BankingOperations.hpp
 void addTransaction(int index, string type, double amount, int otherId = 0) {
     int t = users[index].transactionCount;
     users[index].transactionType[t] = type;
